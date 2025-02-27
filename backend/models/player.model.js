929c5e867e7bec8e0766916cf92a60d412d55386
Sebastian <sdcavallin@gmail.com>
2025-02-27 12:44:53 -0500

    Add socketId to Player model

--- backend/models/player.model.js
+++ backend/models/player.model.js
@@ -45,6 +45,9 @@ export const PlayerSchema = new mongoose.Schema({
   //   type: String,
   //   required: true,
   // },
+  socketId: {
+    type: String,
+  },
 });
 
 const Player = mongoose.model('Player', PlayerSchema);
